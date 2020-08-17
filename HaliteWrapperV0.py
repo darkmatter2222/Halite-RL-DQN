03de986637e935f0e354d2bb764a392ac2fb933d
ryan susman <ryansusman@gmail.com>
2020-08-17 11:14:10 -0500

    checkpoint

--- HaliteWrapperV0.py
+++ HaliteWrapperV0.py
@@ -6,6 +6,8 @@ import abc
 import tensorflow as tf
 import numpy as np
 
+from kaggle_environments import make
+from kaggle_environments.envs.halite.helpers import *
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
@@ -25,111 +27,22 @@ tf.compat.v1.enable_v2_behavior()
 
 class SusmanGameV3(py_environment.PyEnvironment):
     def __init__(self):
-        self.board_width = 12
-        self.board_height = 12
-        self.uuid = str(uuid.uuid1())
-        self.sigma_y = self.board_width / 2
-        self.sigma_x = self.board_height / 2
-        self.channels = 3
-        self.frames = self.board_width * self.board_height
+        # game parms
+        self.board_size = 3
+        self.frames = 400
+        
+        # initialize game
+        self.environment = make("halite", configuration={"size": self.board_size, "startingHalite": 1000})
+        self.agent_count = 1
+        self.environment.reset(self.agent_count)
+
         self._action_spec = array_spec.BoundedArraySpec(
             shape=(), dtype=np.int32, minimum=0, maximum=3, name='action')
         self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(self.frames, self.board_height, self.board_width, self.channels), dtype=np.int32, minimum=0, maximum=1, name='observation')
-        self._state = np.zeros([self.board_height, self.board_width, self.channels])
-        # 0 = Reward Placement
-        # 1 = Player Placement
-        # 2 = Heat Map
-        self._episode_ended = False
-        self.this_turn = 0
-        self.max_turns = self.frames
-        self.total_reward = 0
-        self.heatmap_reward = self.board_height * self.board_width
-        self.player_location = {'y': 0, 'x': 0}
-        self.set_goal()
-        self.game_history = []
-        self.state_history = [self._state] * self.frames
-        self.save_image = False
-        self.image_render_counter = 0
+            shape=(self.frames, self.board_size, self.board_size, self.channels), dtype=np.int32, minimum=0,
+            maximum=1, name='observation')
+        self._state = np.zeros([self.board_size, self.board_size, self.channels])
 
-    def render_image(self, directive='unknown'):
-        new_image = np.zeros([self.board_height, self.board_width, 3])
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        for height in range(self.board_height):
-            for width in range(self.board_width):
-                new_image[height][width] = (0, self._state[height][width][2], 0)
-                if self._state[height][width][0] == 1:
-                    new_image[height][width] = (0, 1, 0)
-                if self._state[height][width][1] == 1:
-                    if directive == 'Exploite':
-                        new_image[height][width] = (1, 0, 0)
-                    elif directive == 'Explore':
-                        new_image[height][width] = (0, 0, 1)
-                    else:
-                        new_image[height][width] = (1, 1, 1)
-
-        n = 15
-        new_image = new_image.repeat(n, axis=0).repeat(n, axis=1)
-        cv2.putText(new_image, self.uuid, (10, 60), font, 1, (0, 0, 1), 2)
-        cv2.imshow('Real Time Play', new_image)
-        cv2.waitKey(1)
-        if self.save_image:
-            matplotlib.image.imsave(f"N:\\Halite\\Images\\{str(self.image_render_counter)}.jpeg", new_image)
-            self.image_render_counter += 1
-
-    def set_goal(self):
-        self.set_player()
-        rand_y = 0
-        rand_x = 0
-
-        while True:
-            if self.board_width > 1:
-                rand_x = random.randrange(0, self.board_width)
-            if self.board_height > 1:
-                rand_y = random.randrange(0, self.board_height)
-            if self.player_location['x'] != rand_x or self.player_location['y'] != rand_y:
-                break
-
-        self._state[rand_y, rand_x][0] = 1
-
-        reward_heatmap = np.zeros([self.board_height, self.board_width])
-        reward_heatmap[rand_y, rand_x] = self.board_height * self.board_width
-        sigma_y = self.board_width / 2
-        sigma_x = self.board_height / 2
-
-        # Apply gaussian filter
-        sigma = [sigma_y, sigma_x]
-        reward_heatmap = sp.ndimage.filters.gaussian_filter(reward_heatmap, sigma, mode='constant')
-
-        for height in range(self.board_height):
-            for width in range(self.board_width):
-                self._state[height][width][2] = reward_heatmap[height, width]
-
-        lol = 1
-
-    def render_new_state(self):
-        for height in range(self.board_height):
-            for width in range(self.board_width):
-                self._state[height, width][1] = 0
-        try:
-            self._state[self.player_location['y'], self.player_location['x']][1] = 1
-        except:
-            lol = 1 # because player can move off map
-
-    def set_player(self):
-        rand_y = 0
-        rand_x = 0
-
-        if self.board_width > 1:
-            rand_x = random.randrange(0, self.board_width)
-        if self.board_height > 1:
-            rand_y = random.randrange(0, self.board_height)
-        self.player_location['x'] = rand_x
-        self.player_location['y'] = rand_y
-        self._state[rand_y, rand_x][1] = 1
-
-    def get_game_history(self):
-        return self.game_history
 
     def action_spec(self):
         return_object = self._action_spec
