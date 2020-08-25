d1b8072cf5e07e9d9baaae228ef783e3eff33a73
ryan susman <ryansusman@gmail.com>
2020-08-24 20:31:50 -0500

    checkpoint

--- susman_rl/dqn_bots/find_the_dot_v0/dqn.py
+++ susman_rl/dqn_bots/find_the_dot_v0/dqn.py
@@ -51,36 +51,33 @@ _eval_py_env = find_the_dot(window_name='Testing')
 
 # wrap the pure python game in a tensorflow wrapper
 _train_env = tf_py_environment.TFPyEnvironment(_train_py_env)
-_train_env._env._envs[0].uuid = 'Training...'
-print('Action Spec:', _train_env.action_spec())
 _eval_env = tf_py_environment.TFPyEnvironment(_eval_py_env)
 
 
 print('Building Network...')
+_fc_layer_params = (512,)
 
-fc_layer_params = (512,)
-
-q_net = q_network.QNetwork(
+_q_net = q_network.QNetwork(
     _train_env.observation_spec(),
     _train_env.action_spec(),
-    fc_layer_params=fc_layer_params)
+    fc_layer_params=_fc_layer_params)
 
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=_learning_rate)
+_optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=_learning_rate)
 
-train_step_counter = tf.Variable(0)
+_train_step_counter = tf.Variable(0)
 
-agent = dqn_agent.DqnAgent(
+_agent = dqn_agent.DqnAgent(
     _train_env.time_step_spec(),
     _train_env.action_spec(),
-    q_network=q_net,
-    optimizer=optimizer,
+    q_network=_q_net,
+    optimizer=_optimizer,
     td_errors_loss_fn=common.element_wise_squared_loss,
-    train_step_counter=train_step_counter)
+    train_step_counter=_train_step_counter)
 
-agent.initialize()
+_agent.initialize()
 
-eval_policy = agent.policy
-collect_policy = agent.collect_policy
+eval_policy = _agent.policy
+collect_policy = _agent.collect_policy
 
 random_policy = random_tf_policy.RandomTFPolicy(_train_env.time_step_spec(),
                                                 _train_env.action_spec())
@@ -103,7 +100,7 @@ def compute_avg_return(environment, policy, num_episodes=10):
 # compute_avg_return(eval_env, random_policy, num_eval_episodes)
 
 replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-    data_spec=agent.collect_data_spec,
+    data_spec=_agent.collect_data_spec,
     batch_size=_train_env.batch_size,
     max_length=_replay_buffer_max_length)
 
@@ -129,25 +126,24 @@ dataset = replay_buffer.as_dataset(
     sample_batch_size=_batch_size,
     num_steps=2).prefetch(3)
 
-agent.train = common.function(agent.train)
+_agent.train = common.function(_agent.train)
 
-agent.train_step_counter.assign(0)
+_agent.train_step_counter.assign(0)
 print('initial collect...')
-avg_return = compute_avg_return(_eval_env, agent.policy, _num_eval_episodes)
+avg_return = compute_avg_return(_eval_env, _agent.policy, _num_eval_episodes)
 returns = [avg_return]
 iterator = iter(dataset)
 
-checkpoint_dir = os.path.join(tempdir, 'checkpoint')
 train_checkpointer = common.Checkpointer(
-    ckpt_dir=checkpoint_dir,
+    ckpt_dir=_checkpoint_policy_dir,
     max_to_keep=1,
-    agent=agent,
-    policy=agent.policy,
+    agent=_agent,
+    policy=_agent.policy,
     replay_buffer=replay_buffer,
-    global_step=train_step_counter
+    global_step=_train_step_counter
 )
-policy_dir = os.path.join(tempdir, 'policy')
-tf_policy_saver = policy_saver.PolicySaver(agent.policy)
+
+tf_policy_saver = policy_saver.PolicySaver(_agent.policy)
 
 restore_network = True
 
@@ -158,21 +154,21 @@ while True:
     print('Training...')
     for _ in tqdm(range(_train_steps)):
         for _ in range(_collect_steps_per_iteration):
-            collect_step(_train_env, agent.collect_policy, replay_buffer)
+            collect_step(_train_env, _agent.collect_policy, replay_buffer)
 
         experience, unused_info = next(iterator)
-        train_loss = agent.train(experience).loss
+        train_loss = _agent.train(experience).loss
 
-        step = agent.train_step_counter.numpy()
+        step = _agent.train_step_counter.numpy()
 
     print('step = {0}: loss = {1}'.format(step, train_loss))
     print('Eval Started...')
-    avg_return = compute_avg_return(_eval_env, agent.policy, _num_eval_episodes)
+    avg_return = compute_avg_return(_eval_env, _agent.policy, _num_eval_episodes)
     returns.append(avg_return)
-    train_checkpointer.save(train_step_counter)
+    train_checkpointer.save(_train_step_counter)
     print('step = {0}: Average Return = {1:.2f}'.format(step, avg_return))
-    tf_policy_saver.save(policy_dir)
+    tf_policy_saver.save(_save_policy_dir)
 
 policy_dir = os.path.join(tempdir, 'policy')
-tf_policy_saver = policy_saver.PolicySaver(agent.policy)
+tf_policy_saver = policy_saver.PolicySaver(_agent.policy)
 tf_policy_saver.save(policy_dir)
